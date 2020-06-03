d1816881ee087962d0dd3ed5fde0ea43538cd0de
Shimoro-Rune <DinaraWolfQuest2013@yandex.ru>
2020-06-03 16:24:11 +0300

    Made GUI for ind. ex. 13

--- Individual exercises/№ 13/Iz 13/Program.fs
+++ Individual exercises/№ 13/Iz 13/Program.fs
@@ -1,25 +1,49 @@
 ﻿open System
+open System.Windows.Forms
+open System.Drawing
+open System.IO
+open System.Text
 
 // Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+//Создание формы
+let form = new Form(Width= 400, Height = 230, Text = "F# Главная форма", Menu = new MainMenu())
 
-let read_data=
-    let rec read_list n=
-        if n=0 then []
-        else
-        let Head = System.Convert.ToInt32(System.Console.ReadLine())
-        let Tail = read_list(n-1)
-        Head::Tail
-    let n=System.Convert.ToInt32(System.Console.ReadLine())
-    read_list n
+//Создание элементов формы
+let button = new Button(Text = "Найти!")
+let textBox1 = new TextBox(Width = 340)
+let textBox2 = new TextBox(Width = 340)
+let label1 = new Label(Text = "Cписок:")
+let label2 = new Label(Text = "Результат:")
 
-let rec write_list (list:int list) =
-    match list with
-    |[] -> let z=System.Console.ReadKey()
-           0
-    |h::t -> System.Console.Write(h.ToString())
-             System.Console.Write(" ")
-             write_list t
+//Задание местоположения элементов формы
+button.Location <- new Point(147, 140)
+textBox1.Location <- new Point (20, 40)
+textBox2.Location <- new Point (20, 100)
+label1.Location <- new Point (20, 20)
+label2.Location <- new Point (20, 80)
+
+//Добавление элементов на форму
+form.Controls.Add(button)
+form.Controls.Add(textBox1)
+form.Controls.Add(textBox2)
+form.Controls.Add(label1)
+form.Controls.Add(label2)
+
+let read_data (text:string) =
+    let rec parse listStr parsedText =
+        match listStr with
+        | h::t -> let number = Convert.ToInt32((string) h)
+                  parse t (List.append parsedText [number])
+        | [] -> parsedText
+
+    parse (List.ofArray (text.Split(' '))) []
+
+let write_list (l) =
+    let mutable gui_textOut = ""
+    let _ = List.iter (fun x -> (gui_textOut <- gui_textOut + " " + x.ToString())) l
+    textBox2.Text<-gui_textOut
+
 
 let sum el =
     let rec s el summa =
@@ -37,5 +61,12 @@ let getDels el =
                           else gD el list (d-1)
     gD el [] el
 
-[<EntryPoint>]
-let main argv = write_list (List.map (fun elem -> List.findIndex (fun el -> el = elem ) read_data) (List.filter (fun elem -> (List.forall (fun el1 -> (sum el1)%(sum elem)=0) read_data) && (List.forall (fun el2 -> List.exists (fun elem -> elem%el2=0) read_data) (getDels elem))) read_data))
+let Click evArgs : unit =
+    let rD = read_data textBox1.Text
+    write_list (List.map (fun elem -> List.findIndex (fun el -> el = elem ) rD) (List.filter (fun elem -> (List.forall (fun el1 -> (sum el1)%(sum elem)=0) rD) && (List.forall (fun el2 -> List.exists (fun elem -> elem%el2=0) rD) (getDels elem))) rD))
+
+//Добавление события для клика по кнопке
+button.Click.Add(Click)
+
+//Запуск формы
+Application.Run(form)
